fbb749b46ef700570b480e0e4e510fc1e1db37e5
richardverheyen <richard.verheyen@gmail.com>
2017-09-07 13:11:00 -0700

    Add the search bar component to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src="../img/logo.svg" className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <SearchBar />
     );
   }
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      address: 'Melbourne, Australia'}
+    this.onChange = (address) => this.setState({ address })
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error))
+  }
+
+  render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange,
+    }
+    const options = {
+      types: ['(regions)']
+    }
+
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <PlacesAutocomplete
+          inputProps={inputProps}
+          options={options}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
+
+export default SearchBar
