03e54741941e28f9984e5a6b5d0ff482ae908c55
richardverheyen <richard.verheyen@gmail.com>
2017-09-10 13:43:20 -0700

    Set up Redux in project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  , document.querySelector('#root')
+);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </AppContainer>
+      , document.querySelector('#root')
+    );
+  });
+}
